ddfdc941de9c3f4239b93dd3e9fc29fb450fa3aa
BryantShepherd <42168740+BryantShepherd@users.noreply.github.com>
2019-09-06 07:02:51 +0700

    commit pc

--- ci-begin-master/src/Human.java
+++ ci-begin-master/src/Human.java
@@ -1,18 +1,23 @@
 import java.lang.reflect.Array;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Human {
+    public static int hourGlassTotal(int[][] arr, int x) {
+        return arr[x][0] + arr[x+1][0];
+    }
     public static void main(String[] args) {
-        ArrayList<String> arrString = new ArrayList<>();
-        System.out.println("Array size: " + arrString.size());
-        arrString.add("h");
-        arrString.add("e");
-        arrString.add("l");
-        System.out.println(arrString.size());
-        System.out.println(arrString);
-        for (int i = 0; i < arrString.size(); i++) {
-            String arrItem = arrString.get(i);
-            System.out.println(arrItem);
+        double d = 12345.678;
+        int[][] arr = new int[6][6];
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                arr[i][j] = i;
+            }
         }
+        NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        System.out.println(hourGlassTotal(arr, 1));
     }
 }
